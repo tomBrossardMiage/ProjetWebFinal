f3516d733b7c2d8ea237613d4be258f31f57ba45
DESKTOP-IOR3PJ0\33778 <tom.brossard@toulouse.miage.fr>
2024-05-12 18:10:01 +0200

    back pour modal fode

--- table.sql
+++ table.sql
@@ -1,4 +1,3 @@
-use recuitMIAGE;
 create table user(
     id_user int primary key AUTO_INCREMENT,
     name varchar(250),
@@ -9,7 +8,6 @@ create table user(
     role varchar(50)
     );
 
-insert into user(name,firstname,pseudo,password,status,role) values('Admin', 'tom', 'admin', 'password', 'true', 'admin');
 CREATE TABLE project (
     id_project INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255),
@@ -19,6 +17,7 @@ CREATE TABLE project (
     date DATE,
     FOREIGN KEY (creator_id) REFERENCES user(id_user)
 );
+
 CREATE TABLE participate (
     id_participate INT AUTO_INCREMENT PRIMARY KEY,
     id_user INT,
